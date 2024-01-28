e3b86bcac08e9a4110b9e7cb06c3d9e80652cc4e
Alexco2003 <alexandru.codarcea@gmail.com>
2024-01-28 20:45:27 +0200

    swagger test

--- Project/Program.cs
+++ Project/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,6 +12,16 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        //options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        //options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
