c1634dc39c6edceabc795da684fc8bf8f4015747
Alexco2003 <alexandru.codarcea@gmail.com>
2024-02-02 23:31:59 +0200

    functional code for the moment

--- Project/Services/UserMovie(Review)Service/UserMovie(Review)Service.cs
+++ Project/Services/UserMovie(Review)Service/UserMovie(Review)Service.cs
@@ -3,8 +3,31 @@ using Project.Repositories.UserMovie_Review_Repository;
 
 namespace Project.Services.UserMovie_Review_Service
 {
-    public class UserMovie_Review_Service: IUserMovie_Review_Service
+    public class UserMovie_Review_Service : IUserMovie_Review_Service
     {
-        
+        public Task AddReview(UserMovie_Review_Create review)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteReview(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserMovie_Review_Formatted2DTO>> GetReviewsOfMovie(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<UserMovie_Review_FormattedDTO>> GetReviewsOfUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateReview(UserMovie_Review_Update review)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
