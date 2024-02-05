ee193f8a87bf5a8d65cdcb23b4f6c84afa6bd808
Alexco2003 <alexandru.codarcea@gmail.com>
2024-02-05 16:54:33 +0200

    working login

--- Project/ClientApp/src/app/log-in-component/log-in-component.component.ts
+++ Project/ClientApp/src/app/log-in-component/log-in-component.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-log-in-component',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./log-in-component.component.css']
 })
 export class LogInComponentComponent {
+  username: string;
+  email: string;
+  password: string;
 
+  constructor(private http: HttpClient, private router: Router) {
+    this.username = '';
+    this.email = '';
+    this.password = '';
+  }
+
+    onSubmit() {
+        this.http.post<{ token: string }>('/api/User/Login', { email: this.email, username: this.username, password: this.password })
+            .subscribe(
+                res => {
+                    // Store the token in the local storage
+                    localStorage.setItem('token', res.token);
+                    // Redirect to the home page
+                    this.router.navigate(['/']);
+                },
+                err => {
+                    // Display the error message
+                    alert(err.error.message);
+                }
+            );
+    }
 }
